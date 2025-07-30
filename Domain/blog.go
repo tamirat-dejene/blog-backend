bc6ac8bd62dfefcda1d410215a5dc962c1c27efc
Tamirat Dejene <tamirat.dejenie@gmail.com>
2025-07-30 17:27:08 +0300

    refactor repository and infrastructure

--- Domain/blog.go
+++ Domain/blog.go
@@ -1,6 +1,9 @@
 package domain
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Blog struct {
 	ID        string
@@ -21,4 +24,36 @@ type BlogComment struct {
 	AuthorID  string
 	Comment   string
 	CreatedAt time.Time
+}
+
+// interface for blog data operations.
+type BlogRepository interface {
+	Create(ctx context.Context, blog *Blog) (*Blog, error)
+	Update(ctx context.Context, id string, blog Blog) (Blog, error)
+	Delete(ctx context.Context, id string) error
+	FindAll(ctx context.Context) ([]Blog, error)
+
+	// Find related blogs which title relates to the given title
+	FindByTitle(ctx context.Context, title string) ([]Blog, error)
+
+	//... more methods can be added based on the usecases
+}
+
+type BlogCommentRepository interface {
+	Create(ctx context.Context, comment BlogComment) (Blog, error)
+	Delete(ctx context.Context, id string) error
+}
+
+// interface for blog data operations
+type BlogUsecase interface {
+	CreateBlog(ctx context.Context, blog *Blog) (*Blog, error)
+	UpdateBlog(ctx context.Context, id string, blog Blog) (Blog, error)
+	DeleteBlog(ctx context.Context, id string) error
+	GetAllBlogs(ctx context.Context) ([]Blog, error)
+	GetBlogsByTitle(ctx context.Context, title string) ([]Blog, error)
+}
+
+type BlogCommentUsecase interface {
+	CreateComment(ctx context.Context, comment BlogComment) (Blog, error)
+	DeleteComment(ctx context.Context, id string) error
 }
--- Usecases/blog_usecase.go
+++ Usecases/blog_usecase.go
@@ -2,11 +2,48 @@
 package usecases
 
 import (
-	repositories "g6/blog-api/Repositories"
+	"context"
+	domain "g6/blog-api/Domain"
 	"time"
 )
 
 type blogUsecase struct {
-	blogRepo repositories.BlogRepository
-	ctxtimeout  time.Duration
+	blogRepo   domain.BlogRepository
+	ctxtimeout time.Duration
+}
+
+// CreateBlog implements domain.BlogUsecase.
+func (b *blogUsecase) CreateBlog(ctx context.Context, blog *domain.Blog) (*domain.Blog, error) {
+	// example implement
+	c, cancel := context.WithTimeout(ctx, b.ctxtimeout)
+	defer cancel()
+
+	return b.blogRepo.Create(c, blog)
+}
+
+// DeleteBlog implements domain.BlogUsecase.
+func (b *blogUsecase) DeleteBlog(ctx context.Context, id string) error {
+	panic("unimplemented")
+}
+
+// GetAllBlogs implements domain.BlogUsecase.
+func (b *blogUsecase) GetAllBlogs(ctx context.Context) ([]domain.Blog, error) {
+	panic("unimplemented")
+}
+
+// GetBlogsByTitle implements domain.BlogUsecase.
+func (b *blogUsecase) GetBlogsByTitle(ctx context.Context, title string) ([]domain.Blog, error) {
+	panic("unimplemented")
+}
+
+// UpdateBlog implements domain.BlogUsecase.
+func (b *blogUsecase) UpdateBlog(ctx context.Context, id string, blog domain.Blog) (domain.Blog, error) {
+	panic("unimplemented")
+}
+
+func NewBlogUsecase(blogRepo domain.BlogRepository, timeout time.Duration) domain.BlogUsecase {
+	return &blogUsecase{
+		blogRepo:   blogRepo,
+		ctxtimeout: timeout,
+	}
 }
