d7e838df7ad94d1a3e6b5d4c658e813e31854ae0
raodenn <158840047+raodenn@users.noreply.github.com>
2025-08-01 14:52:09 +0300

    implemented blog usecases

--- Usecases/blog_comment_usecase.go
+++ Usecases/blog_comment_usecase.go
@@ -1,1 +1,34 @@
 package usecases
+
+import (
+	"context"
+	domain "g6/blog-api/Domain"
+	"time"
+)
+
+type blogCommentUsecase struct {
+	blogCommentRepo domain.BlogCommentRepository
+	ctxtimeout      time.Duration
+}
+
+func (b *blogCommentUsecase) CreateComment(ctx context.Context, comment domain.BlogComment) (domain.BlogComment, error) {
+	c, cancel := context.WithTimeout(ctx, b.ctxtimeout)
+	defer cancel()
+
+	return b.blogCommentRepo.Create(c, comment)
+}
+
+func (b *blogCommentUsecase) DeleteComment(ctx context.Context, id string) error {
+	c, cancel := context.WithTimeout(ctx, b.ctxtimeout)
+	defer cancel()
+
+	return b.blogCommentRepo.Delete(c, id)
+}
+
+func NewBlogCommentUseCase(blogCommentRepo domain.BlogCommentRepository, timeout time.Duration) domain.BlogCommentUsecase {
+	return &blogCommentUsecase{
+		blogCommentRepo: blogCommentRepo,
+		ctxtimeout:      timeout,
+	}
+
+}
